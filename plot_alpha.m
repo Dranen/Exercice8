3af93d95b3f23eb3739966e4f8c86fa83f37bb34
Simon Eggenschwiler <simoneggenschwiler@tsf-428-wpa-5-036.epfl.ch>
2015-05-26 13:38:06 +0200

    ajout plot alpha

--- plot_alpha.m
+++ plot_alpha.m
@@ -3,4 +3,8 @@
 bon=load('max_noy_alpha.dat');
 
 figure;
-plot(bon(:,1),bon(:,2));
+plot(bon(:,1),bon(:,2),'+');
+hold on;
+set(gca,'fontsize',20);
+xlabel('alpha');
+ylabel('max \psi^2 \in [R_{nucleus},0]');
